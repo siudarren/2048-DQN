9289f983679ef703e55aeed74f6d22d476ba220d
Darren <mcsiu@berkeley.edu>
2026-01-02 13:18:28 +0800

    Cleanup tracked frontend files, keep only dist

--- Frontend/src/App.jsx
+++
@@ -1,8 +0,0 @@
-// src/App.js
-import React from "react";
-import GameContainer from "./Component/GameContainer";
-function App() {
-    return <GameContainer style={{margin: "0 auto"}}></GameContainer>;
-}
-
-export default App;
